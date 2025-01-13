cd1b0fa3bc0bdf2a3b182c905fe6aef3e4e923c1
Androsh7 <150483650+Androsh7@users.noreply.github.com>
2025-01-12 21:36:03 -0700

    Added license to main file

--- clock.cpp
+++ clock.cpp
@@ -7,6 +7,34 @@
 
 #pragma comment(lib, "User32.lib") // tells compiler to link user32.lib for keyboard reading
 
+/*
+CLI_Clock by Androsh7
+https://github.com/Androsh7/CLI_Clock
+
+MIT License
+
+Copyright (c) 2025 Androsh7
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
 // sets the coordinate position for the cursor
 void setCursorPos(int x, int y) {
     printf("\033[%d;%dH", y,x);
